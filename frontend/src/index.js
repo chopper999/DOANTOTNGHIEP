7b6de6d450d1799babfa9159fbd8a24b9b87de6d
zZChopperZz <vanspykid85@gmail.com>
2021-05-21 11:42:19 +0700

    push to keroku

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 import 'semantic-ui-css/semantic.min.css'
 
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
